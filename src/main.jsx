40f6ebea3ab9d92549d9ef7875d75bcce609bb81
Damilare Tolulope <damilaretolulope1@gmail.com>
2023-08-25 12:25:36 +0100

    chore: add react toast

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { ToastProvider } from 'react-toast-notifications';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ToastProvider autoDismiss autoDismissTimeout={5000}>
+        <App />
+      </ToastProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
